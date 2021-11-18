48e2a0ebe66dbeadbf3bc47cd9da6a6c5ddfc397
baudouinMartelee <48315946+baudouinMartelee@users.noreply.github.com>
2021-11-18 17:25:00 +0100

    test file updated

--- 22/Test_22_JAB.py
+++ 22/Test_22_JAB.py
@@ -1,17 +1,13 @@
-from run_22_JAB import *
 
-#we use weights calculated in the run file
-W = result[1]
-N=32
-sigma = 1.0
-rho = 0.0009
+import NeuralNetwork as nn
+import numpy as np
 
-def ICanGeneralize(X) :
+
+def ICanGeneralize(X, optimizedWeights) :
     X = np.array(X)
     X1 = X[:,0]
     X2 = X[:,1]
     mixed =  np.vstack((X1.flatten(),X2.flatten())).T
-    a,b,res = opt.preproc(N,mixed)
-    Y_pred = opt.func(W, (N, sigma,res,None, rho ))
-    
+    Y_pred = nn.prediction(optimizedWeights, mixed)
+
     return Y_pred
