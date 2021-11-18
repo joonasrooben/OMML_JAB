76e3a84e68a8d6e27f3aaf3356e0ca18a6a76f8a
aurelebartolomeo <aurele.bartolomeo@student.uclouvain.be>
2021-11-18 19:13:01 +0100

    test files bug fix

--- 11/Test_11_JAB.py
+++ 11/Test_11_JAB.py
@@ -1,13 +1,16 @@
 
-import NeuralNetwork as nn
-import numpy as np
+from run_11_JAB import *
 
+#we use weights calculated in the run file
+W = optimized_weights
+N=32
+sigma = 1.5
+rho = 0.0007
 
-def ICanGeneralize(X, optimizedWeights) :
+def ICanGeneralize(X) :
     X = np.array(X)
     X1 = X[:,0]
     X2 = X[:,1]
     mixed =  np.vstack((X1.flatten(),X2.flatten())).T
-    Y_pred = nn.prediction(optimizedWeights, mixed)
-
+    Y_pred = nn.prediction(W, mixed)
     return Y_pred
--- 21/Test_21_JAB.py
+++ 21/Test_21_JAB.py
@@ -1,2 +1,15 @@
+from run_21_JAB import *
+
+#we use weights calculated in the run file
+W = optimized_weights
+N=32
+sigma = 1.5
+rho = 0.0007
+
 def ICanGeneralize(X) :
-    return X
+    X = np.array(X)
+    X1 = X[:,0]
+    X2 = X[:,1]
+    mixed =  np.vstack((X1.flatten(),X2.flatten())).T
+    Y_pred = nn.prediction(W, mixed)
+    return Y_pred
